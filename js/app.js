c9cf4f9db960d631aeaff2cac917250a731f5528
Jordan Johnson <josamjohn@gmail.com>
2014-02-15 21:56:35 -0600

    first commit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$("#ryan-info").hover(
+  function() {
+    $(this).addClass( "hover-box" );
+  }, function() {
+    $(this).removeClass( "hover-box" );
+  }
+);
